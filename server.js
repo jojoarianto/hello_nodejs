84f4e7d9f6a721ed2c99cbbf1c5e7908279315d4
jojoarianto <irianto94@outlook.com>
2016-02-23 20:14:41 +0700

    demo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 // app.get('/', function(req, res){
 // 	res.send('Hello World!');
@@ -7,6 +10,10 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, function(){
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function(){
 	console.log('listening app in port');
 });
